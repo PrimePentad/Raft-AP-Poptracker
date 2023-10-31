07a328d6b999931c68a8c4ffd9b123bba56be05e
PrimePentad <121827409+PrimePentad@users.noreply.github.com>
2023-10-31 15:19:29 -0500

    Update init.lua

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,19 +1,5 @@
--- entry point for all lua code of the pack
--- more info on the lua API: https://github.com/black-sliver/PopTracker/blob/master/doc/PACKS.md#lua-interface
-ENABLE_DEBUG_LOG = true
--- get current variant
-local variant = Tracker.ActiveVariantUID
--- check variant info
+
 IS_ITEMS_ONLY = variant:find("itemsonly")
-
-print("-- Example Tracker --")
-print("Loaded variant: ", variant)
-if ENABLE_DEBUG_LOG then
-    print("Debug logging is enabled!")
-end
-
--- Utility Script for helper functions etc.
-ScriptHost:LoadScript("scripts/utils.lua")
 
 -- Logic
 ScriptHost:LoadScript("scripts/logic/logic.lua")
