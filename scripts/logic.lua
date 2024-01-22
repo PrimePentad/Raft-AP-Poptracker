c46e173b3abf140d278705b4b8f6c1c603d72ae5
PrimePentad <121827409+PrimePentad@users.noreply.github.com>
2024-01-22 13:25:05 -0600

    Out of logic updates
    Navigate/Drive
    Shovel/Dirt/Treasure Hunt
    Animal Capture

--- scripts/logic.lua
+++ scripts/logic.lua
@@ -41,17 +41,25 @@ function can_cut()
 end
 
 function has_zipline()
-    return (has("zipline_tool))
+    return (has("zipline_tool"))
 end
 
 function can_zipline()
     return (has_zipline() and has_smelter() and has_bolt() and has_hinge())
 end
 
+function can_early_navigate()
+    return (has("battery") and has("receiver") and has("antenna") and 
+        has("smelter") and has("circuit_board"))
+
 function can_navigate()
     return (has("battery") and has("receiver") and has("antenna") and 
         has("smelter") and has("bolt") and has("hinge") and
         has("circuit_board"))
+end
+
+function can_early_drive()
+    return (can_early_navigate() and ((has("engine") and has("steering_wheel")) or has("paddleboard_mode")))
 end
 
 function can_drive()
@@ -68,23 +76,6 @@ end
 
 function has_explosive_powder()
     return (can_access_big_islands() and has_smelter())
-end
-    
-function can_shovel()
-    return (has("shovel") and has_smelter() and has_bolt())
-end
-    
-function has_dirt()
-    return (can_access_big_islands() and can_shovel())
-end
-
-function can_treasure_hunt()
-    return (can_shovel() and has("battery") and has("metal_detector"))
-end
-            
-function can_capture_animals()
-    return (can_access_big_islands() and has("grass_plot") and 
-        has("net_launcher") and has("net_canister") and has_smelter())
 end
 
 function can_access_vasagatan()
@@ -113,4 +104,62 @@ end
 
 function can_access_utopia()
     return (can_drive() and has("utopia_frequency"))
+end
+
+
+function can_access_vasagatan_early()
+    return (can_early_navigate() and has("vasagatan_frequency"))
+end
+
+function can_access_balboa_early()
+    return (can_early_drive() and has("balboa_frequency"))
+end
+
+function can_access_caravan_town_early()
+    return (can_early_drive() and has("caravan_town_frequency"))
+end
+
+function can_access_tangaroa_early()
+    return (can_early_drive() and has("tangaroa_frequency"))
+end
+
+function can_access_varuna_point_early()
+    return (can_early_drive() and has("varuna_point_frequency"))
+end
+
+function can_access_temperance_early()
+    return (can_early_drive() and has("temperance_frequency"))
+end
+
+function can_access_utopia_early()
+    return (can_early_drive() and has("utopia_frequency"))
+end
+
+    
+function can_early_shovel()
+    return ((has("shovel") and has_smelter()) or can_access_utopia_early() or can_access_utopia())
+end
+    
+function can_shovel()
+    return (has("shovel") and has_smelter() and has_bolt())
+end
+
+function has__early_dirt()
+    return (can_access_big_islands() and can_early_shovel())
+end
+
+function has_dirt()
+    return (can_access_big_islands() and can_shovel())
+end
+
+function can_early_treasure_hunt()
+    return (can_early_shovel() and has("battery") and has("metal_detector"))
+end
+
+function can_treasure_hunt()
+    return (can_shovel() and has("battery") and has("metal_detector"))
+end
+            
+function can_capture_animals()
+    return (can_access_big_islands() and has("net_launcher") and has("net_canister") and has_smelter())
 end
