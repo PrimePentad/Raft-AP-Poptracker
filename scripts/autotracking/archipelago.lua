a2238a029bb12e15694c85d9ba4ed6b38a58d18d
PrimePentad <121827409+PrimePentad@users.noreply.github.com>
2024-01-30 18:11:45 -0600

    Update archipelago.lua

--- scripts/autotracking/archipelago.lua
+++ scripts/autotracking/archipelago.lua
@@ -277,7 +277,7 @@ function onLocation(location_id, location_name)
         print(string.format("onLocation: could not find object for code %s", v[1]))
     end
 
-    if item_id == 48155 then   
+    if location_id == 48155 then   
         local relay_quest = Tracker:FindObjectForCode("relay_station_quest_item")
         relay_quest.Active = true
     end
